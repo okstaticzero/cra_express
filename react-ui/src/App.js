8069ab538c23bc24620fad5ff597f636ccf27b36
Matt Wallace <matthew.wallace@cognizant.com>
2017-10-10 21:22:17 -0600

    added two

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React, one, two</h2>
+          <h2>Welcome to React, one, two, three</h2>
           <p>Hi there</p>
         </div>
         <p className="App-intro">
